17c2984788acd4b279d8a5385b42c880decbcdf9
arunkumar-dash <arun.kumar.dash77@gmail.com>
2022-01-03 15:36:32 +0530

    functions to change defaults

---
+++ ReminderCommandLineApp/NotesController.swift
@@ -0,0 +1,27 @@
+//
+//  NotesController.swift
+//  ReminderCommandLineApp
+//
+//  Created by Arun Kumar on 03/01/22.
+//
+
+import Foundation
+
+struct NotesController {
+    
+    func add() {
+        
+    }
+    
+    func view() {
+        
+    }
+    
+    func edit() {
+        
+    }
+    
+    func changePreferences() {
+        
+    }
+}
